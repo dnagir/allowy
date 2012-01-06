1fadfc2df5716f122d5f7d2de12fc4929e4b4edb
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-06 21:47:47 +1100

    move the files into proper lib folder

--- lib/allowy.rb
+++ lib/allowy.rb
@@ -1,5 +1,47 @@
 require "allowy/version"
+require 'active_support/concern'
 
 module Allowy
-  # Your code goes here...
+  class UndefinedActionError < StandardError; end
+
+  module AccessControl
+    extend ActiveSupport::Concern
+    included do
+      attr_accessor :context
+    end
+
+    module ClassMethods
+      def context(*accessors)
+        accessors.each do |m|
+          attr_accessor m
+        end
+      end
+    end
+
+    module InstanceMethods
+      def initialize(context={})
+        prepare_context(context)
+      end
+
+      def prepare_context(context={})
+        @context = context
+        context.each_pair do |k,v|
+          self.send("#{k}=", v)
+        end
+        self
+      end
+
+      def can?(action, subject)
+        raise UndefinedActionError.new unless self.respond_to? action
+        send(action, subject)
+      end
+
+      def cannot?(action, subject)
+        !can?(action, subject)
+      end
+
+    end
+
+  end
+
 end
---
+++ lib/allowy/matchers.rb
@@ -0,0 +1,44 @@
+module Allowy
+  module Matchers
+
+    class AbleToMatcher
+      def initialize(action, subject)
+        @action, @subject = action, subject
+      end
+
+      def say msg
+        "#{msg} #{@action} #{@subject.inspect}" + if @context && @context.any?
+          ' ' + @context.inspect
+        else
+          ''
+        end
+      end
+
+      def matches?(access_control)
+        @context = access_control.context
+        access_control.can?(@action, @subject)
+      end
+
+      def description
+        say "be able to"
+      end
+
+      def failure_message
+        say "expected to be able to"
+      end
+
+      def negative_failure_message
+        say "expected NOT to be able to"
+      end
+    end
+
+    ::RSpec::Matchers.define :be_able_to do |*args|
+      m = AbleToMatcher.new(*args)
+      match                           {|a| m.matches?(a) }
+      failure_message_for_should      { m.failure_message }
+      failure_message_for_should_not  { m.negative_failure_message }
+      description                     { m.description }
+    end
+
+  end
+end
