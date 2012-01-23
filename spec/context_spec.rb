85298c7530963c5fa754f11dda254754fb369ba8
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-23 19:21:01 +1100

    silly, forgot to delete the implementation from the spec

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -1,33 +1,6 @@
 require 'spec_helper'
 
 module Allowy
-  module Context
-    extend ActiveSupport::Concern
-
-    module InstanceMethods
-
-      def allowy_context
-        self
-      end
-
-      def current_allowy
-        @current_allowy ||= ::Allowy::Registry.new(allowy_context)
-      end
-
-      def can?(action, subject, *args)
-        current_allowy.access_control_for!(subject).can?(action, subject, *args)
-      end
-
-      def cannot?(*args)
-        current_allowy.access_control_for!(subject).cannot?(action, subject, *args)
-      end
-
-      def authorize!(action, subject, *args)
-        current_allowy.access_control_for!(subject).authorize!(action, subject, *args)
-      end
-    end
-  end
-
   class SampleContext
     include Context
   end
